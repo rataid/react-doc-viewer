317a312e820bc42523c175ad9e01c4bc869923d1
Jojo Narte <joselitonarte@gmail.com>
2021-03-03 13:55:36 +0800

    feature: added no-renderer override

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,49 +1,57 @@
-import { FC, ReactElement } from "react";
-import { ThemedStyledProps } from "styled-components";
-import { IMainState } from "../state/reducer";
-import { FileLoaderFunction } from "../utils/fileLoaders";
+import { FC, ReactElement } from 'react';
+import { ThemedStyledProps } from 'styled-components';
+import { IMainState } from '../state/reducer';
+import { FileLoaderFunction } from '../utils/fileLoaders';
 
 export interface IConfig {
-  header?: IHeaderConfig;
+	header?: IHeaderConfig;
+	noRenderer: INoRendererConfig;
 }
 export interface IHeaderConfig {
-  disableHeader?: boolean;
-  disableFileName?: boolean;
-  retainURLParams?: boolean;
-  overrideComponent?: IHeaderOverride;
+	disableHeader?: boolean;
+	disableFileName?: boolean;
+	retainURLParams?: boolean;
+	overrideComponent?: IHeaderOverride;
+}
+export interface INoRendererConfig {
+	overrideComponent?: INoRendererOverride;
 }
 
 export type IHeaderOverride = (
-  state: IMainState,
-  previousDocument: () => void,
-  nextDocument: () => void
+	state: IMainState,
+	previousDocument: () => void,
+	nextDocument: () => void
+) => ReactElement<any, any> | null;
+
+export type INoRendererOverride = (
+	state: IMainState
 ) => ReactElement<any, any> | null;
 
 export interface ITheme {
-  primary?: string;
-  secondary?: string;
-  tertiary?: string;
-  text_primary?: string;
-  text_secondary?: string;
-  text_tertiary?: string;
-  disableThemeScrollbar?: boolean;
+	primary?: string;
+	secondary?: string;
+	tertiary?: string;
+	text_primary?: string;
+	text_secondary?: string;
+	text_tertiary?: string;
+	disableThemeScrollbar?: boolean;
 }
 
 export interface IStyledProps extends ThemedStyledProps<any, any> {
-  theme: ITheme;
+	theme: ITheme;
 }
 
 export interface IDocument {
-  uri: string;
-  fileType?: string;
-  fileData?: string | ArrayBuffer;
+	uri: string;
+	fileType?: string;
+	fileData?: string | ArrayBuffer;
 }
 
 export interface DocRendererProps {
-  mainState: IMainState;
+	mainState: IMainState;
 }
 export interface DocRenderer extends FC<DocRendererProps> {
-  fileTypes: string[];
-  weight: number;
-  fileLoader?: FileLoaderFunction | null | undefined;
+	fileTypes: string[];
+	weight: number;
+	fileLoader?: FileLoaderFunction | null | undefined;
 }
