23b66b3894090d81b5f7b646236b02ee723451a0
Jojo Narte <joselitonarte@gmail.com>
2021-03-23 23:01:32 +0800

    fix: png renderer

--- src/plugins/png/index.tsx
+++ src/plugins/png/index.tsx
@@ -11,6 +11,11 @@ const StyledImageRenderer = styled(ImageProxyRenderer)`
 	height: 100%;
 	background-color: transparent;
 	background-size: 20px 20px;
+	background-image: linear-gradient(45deg, #e0e0e0 25%, transparent 25%),
+		linear-gradient(-45deg, #e0e0e0 25%, transparent 25%),
+		linear-gradient(45deg, transparent 75%, #e0e0e0 75%),
+		linear-gradient(-45deg, transparent 75%, #e0e0e0 75%);
+	background-size: 20px 20px;
 	background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
 `;
 
